740d71b41f1ba569823e9ac70c6b000ec9d67182
Albert Xhani <albert.xhani@outlook.com>
2022-02-14 20:57:12 +0100

    RedisConnector class

---
+++ src/RedisConnector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Quechedra;
+
+class RedisConnector
+{
+
+    /**
+     * Redis instance
+     *
+     * @var Redis
+     */
+    private $instance = null;
+
+
+    function __construct()
+    {
+        if(!class_exists("Redis")) {
+            throw new \Exception("Redis extenstion is not activated");
+        }
+    }
+
+    /**
+     * Create a new redis connection
+     *
+     * @param array $config Connection details
+     *
+     * @return Redis
+     */
+    public function create($config)
+    {
+
+        if ($this->instance) return $this->instance;
+
+        $redis = new \Redis();
+        $redis->connect(...array_values($config));
+
+        return $this->instance = $redis;
+    }
+
+}
