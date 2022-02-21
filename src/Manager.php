1b38d4b7f38b4c150e4b85d5dd45d8c4176a6633
Albert Xhani <albert.xhani@outlook.com>
2022-02-21 16:53:35 +0100

    Manager - pop job from queues

--- src/Manager.php
+++ src/Manager.php
@@ -11,11 +11,42 @@ class Manager
         $this->connection = Client::getInstance()->connection;
     }
 
+    /**
+     * Push a job into the queue
+     *
+     * @param array $payload Job's payload
+     *
+     * @return void
+     */
     public function push($payload)
     {
         $queue = $payload["queue"];
         
         $this->connection->sAdd("queues", $queue);
         $this->connection->lPush("queue:{$queue}", json_encode($payload));
+    }
+
+    /**
+     * Pop a job from the available queues
+     *
+     * @param boolean $queues
+     *
+     * @return atring
+     */
+    public function pop($queues = false)
+    {
+        $queues = $queues ?? $this->getQueues();
+        return $this->connection->brPop($queues);
+    }
+
+    /**
+     * Get all queues saved on redis
+     *
+     * @return void
+     */
+    private function getQueues()
+    {
+        $queues = $this->connection->sMembers('queues');
+        return \shuffle($queues);
     }
 }
