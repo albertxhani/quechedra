81536358ae43e8bd2591f948a5c0f1f1f1ef7d91
Albert Xhani <albert.xhani@outlook.com>
2022-02-25 18:26:08 +0100

    Logger - evaluation of logging levels

--- src/Logger.php
+++ src/Logger.php
@@ -5,19 +5,70 @@ namespace Quechedra;
 class Logger
 {
 
-    private $strean;
+    private $stream;
 
     private $level;
 
+    /**
+     * Available log levels
+     *
+     * @var array
+     */
+    private $levels = [
+        "fatal",
+        "error",
+        "warn",
+        "info",
+        "debug"
+    ];
+
+    /**
+     * Initialize Logger
+     *
+     * @param mixed  $stream Output Stream
+     * @param string $level  Log Level
+     */
     public function __construct($stream, $level)
     {
         $this->stream = $stream;
         $this->level = $level;
     }
 
-    public function log($content)
+    /**
+     * Log message by level. If the log level is lower in
+     * the hierarchy than the default logging level
+     * the message will not be logged
+     *
+     * @param string $content Message to be logged
+     * @param string $level   on of the available levels
+     *
+     * @return void
+     */
+    public function log($content, $level = "debug")
     {
-        $this->streamer->log($content);
+        if(!in_array($level, $this->levels)) {
+            return;
+        }
+
+        if($this->evaluate($level)) {
+            $this->stream->$level($content);
+        }
     }
 
+    /**
+     * Evaluate if the message should be logged or not
+     * based on applications configuration. Check
+     * quechedra.yml for more
+     *
+     * @param string $level One of the available log levels
+     *
+     * @return void
+     */
+    private function evaluate($level)
+    {
+        return (
+            array_search($level, $this->levels)
+             >= array_search($this->level, $this->levels)
+        );
+    }
 }
